bf8b9b04d9a34e297c8c15bea126c66ebbc6db5a
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-09-23 03:13:18 +1000

    chore: Update version number to 2.2 in build configuration and localization files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.llmhub.llmhub"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "2.1.1"
+        versionCode = 17
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val hfToken: String = localProperties.getProperty("HF_TOKEN", "")
