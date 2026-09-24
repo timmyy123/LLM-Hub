37b4f8e7f7071216c9c3cd686e7f9fec76f36c3d
Timmy Qian <164847362+timmyy123@users.noreply.github.com>
2026-09-25 04:13:51 +1000

    chore: bump app version to 4.3.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.llmhub.llmhub"
         minSdk = 27
         targetSdk = 37
-        versionCode = 153
-        versionName = "4.3.4"
+        versionCode = 154
+        versionName = "4.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val hfToken: String = localProperties.getProperty("HF_TOKEN", "")
